2f76ebd4b74aefd15419e2c4405d3565b253e954
David <david.windler@windowslive.com>
2021-11-10 17:47:07 +0100

    Added powercoders CSS

--- slides/reveal-setup.js
+++ slides/reveal-setup.js
@@ -1,6 +1,6 @@
 import 'reveal.js/dist/reveal.css';
-import 'reveal.js/dist/theme/white.css';
 import 'reveal.js/plugin/highlight/monokai.css';
+import './powercoders.css';
 
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
