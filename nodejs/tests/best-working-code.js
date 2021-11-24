72c25538c87ae9de4c126fbd2220477fcde99a36
David <david.windler@windowslive.com>
2021-11-24 13:04:16 +0100

    add node js slides and exercises

---
+++ nodejs/tests/best-working-code.js
@@ -0,0 +1,11 @@
+const sum = (a, b) => a + b;
+
+const toUpperCase = (str) => str.toLowerCase();
+
+const insertAtFirstPosition = (element, array) => [...array, element];
+
+module.exports = {
+  sum,
+  toUpperCase,
+  insertAtFirstPosition,
+};
